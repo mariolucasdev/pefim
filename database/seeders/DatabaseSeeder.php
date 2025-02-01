0e5f6701fdd45be86bb00462bf3666c6c00c2d9a
Mário Lucas <mariolucasdev@gmail.com>
2025-02-01 17:47:19 -0300

    feat: initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Ramsey\Uuid\Uuid;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'id' => Uuid::uuid4()->toString(),
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
